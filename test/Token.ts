04b1ebba17e1c0fe04dc215e837d1d4bc8411f32
Jonathan Morales Velez <jonathan.morales-velez@spendesk.com>
2023-01-29 18:19:17 +0100

    deploy token to a fixture

--- test/Token.ts
+++ test/Token.ts
@@ -1,22 +1,26 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Token contract", () => {
-    it("Deployment should assign the total supply of tokens to the owner", async () => {
-        const [owner] = await ethers.getSigners();
+    async function deployTokenFixture() {
+        const [owner, accountOne, accountTwo] = await ethers.getSigners();
         const Token = await ethers.getContractFactory("Token");
-        const jonathanmvToken = await Token.deploy();
 
+        const jonathanmvToken = await Token.deploy();
+        await jonathanmvToken.deployed();
+
+        return { Token, jonathanmvToken, owner, accountOne, accountTwo };
+    }
+
+    it("Deployment should assign the total supply of tokens to the owner", async () => {
+        const { jonathanmvToken, owner } = await loadFixture(deployTokenFixture);
         const ownerBalance = await jonathanmvToken.balanceOf(owner.address);
         expect(await jonathanmvToken.totalSupply()).to.equal(ownerBalance);
     });
-
+    
     it("Should transfer tokens between accounts", async () => {
-        const [owner, accountOne, accountTwo] = await ethers.getSigners();
-        
-        const Token = await ethers.getContractFactory("Token");
-        const jonathanmvToken = await Token.deploy();
-
+        const { jonathanmvToken, owner, accountOne, accountTwo } = await loadFixture(deployTokenFixture);
         // from owner to accountOne
         await jonathanmvToken.transfer(accountOne.address, 50);
         expect(await jonathanmvToken.balanceOf(owner.address)).to.equal((await jonathanmvToken.totalSupply()).toNumber() - 50);
@@ -27,5 +31,7 @@ describe("Token contract", () => {
         await jonathanmvToken.transfer(accountTwo.address, 25);
         expect(await jonathanmvToken.balanceOf(accountTwo.address)).to.equal(25);
 
+        jonathanmvToken.connect(accountTwo);
+        expect(await jonathanmvToken.transfer(owner.address, 5)).to.changeTokenBalances(jonathanmvToken, [accountTwo, owner], [-5, 5]);
     });
 });
